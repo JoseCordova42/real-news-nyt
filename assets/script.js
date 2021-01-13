f6d0c0e0f6588a870f01bfc4da5ae82c53f2c5d1
Stephen Price <stephen_lprice@live.com>
2021-01-13 09:58:54 -0600

    initial stubs for the Ajax request

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,35 @@ var search = $("#search");
 var records = $("#numRecords");
 var start = $("#startYear");
 var end = $("#endYear");
-var topArticles = $("#topArticles")
+var topArticles = $("#topArticles");
 
+
+// Click on search makes an AJAX request to the NYT API
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+    var begin_date = "begin_date=" + daveVar0;
+    var end_date = "&end_date=" + daveVar1;
+    var query = "&q=" + daveVar2;
+    var key = "&api-key=e0aXsblILsvDExrjmOsKLkG7fL4UyRkH";
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20200101&end_date=20210112&q=covid&api-key=e0aXsblILsvDExrjmOsKLkG7fL4UyRkH",
+        "method": "GET",
+        "headers": {}
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
     
 })
 
+// Resets all the variables to be empty
 $("#clearBtn").on("click", function(event) {
     event.preventDefault();
     search.empty();
@@ -17,3 +39,8 @@ $("#clearBtn").on("click", function(event) {
     end.empty();
     topArticles.empty();
 })
+
+// AJAX request
+
+
+
